fcbfabaf20896fc35690d5ded35e2a740e4f04ee
Arkanyga <Arkanyga1@mail.ru>
2018-12-18 13:13:40 +0500

    step 15
    машина застревает в стене

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ const carPic = document.createElement("img"),
   TRACK_PLAYER = 2;
 
 
-let carX = 75, carY = 75,
+let carX, carY,
   carSpeed = 0,
   startCarAng = Math.PI,
   carAng = -0.5 * Math.PI,
@@ -145,20 +145,26 @@ function calculateMousePos(e) {
 }
 
 function moveEverething() {
-  if (keyHeldGas) {
-    carSpeed += DRIVE_POWER;
+  let nextCarX = carX + Math.cos(carAng) * carSpeed;
+  let nextCarY = carY + Math.sin(carAng) * carSpeed;
+  if (checkForTrackAtPixelCoord(nextCarX, nextCarY)) {
+    if (keyHeldGas) {
+      carSpeed += DRIVE_POWER;
+    }
+    if (keyHeldReverse) {
+      carSpeed -= REVERSE_POWER;
+    }
+    if (keyHeldTurnLeft && Math.abs(carSpeed) > MIN_TURN_SPEED) {
+      carAng -= TURN_RATE * Math.PI;
+    }
+    if (keyHeldTurnRight && Math.abs(carSpeed) > MIN_TURN_SPEED) {
+      carAng += TURN_RATE * Math.PI;
+    }
+    carX = nextCarX;
+    carY = nextCarY;
+  } else {
+    carSpeed = 0;
   }
-  if (keyHeldReverse) {
-    carSpeed -= REVERSE_POWER;
-  }
-  if (keyHeldTurnLeft && Math.abs(carSpeed) > MIN_TURN_SPEED) {
-    carAng -= TURN_RATE * Math.PI;
-  }
-  if (keyHeldTurnRight && Math.abs(carSpeed) > MIN_TURN_SPEED) {
-    carAng += TURN_RATE * Math.PI;
-  }
-  carX += Math.cos(carAng) * carSpeed;
-  carY += Math.sin(carAng) * carSpeed;
   carSpeed *= GROUNDSPEED_DECAY_MULT;
 }
 
@@ -178,7 +184,7 @@ function carReset() {
 function drawTracks() {
   for (let col = 0; col < TRACK_COLS; col++) {
     for (let row = 0; row < TRACK_ROWS; row++) {
-      if (isTrackAtTileCoord(col, row)) {
+      if (isWallAtTileCoord(col, row)) {
         let trackLeftEdgeX = col * TRACK_W;
         let trackTopEdgeY = row * TRACK_H;
         colorRect(trackLeftEdgeX, trackTopEdgeY, TRACK_W - TRACK_GAP,
@@ -190,53 +196,24 @@ function drawTracks() {
 
 
 
-function isTrackAtTileCoord(trackTileCol, trackTileRow) {
+function isWallAtTileCoord(trackTileCol, trackTileRow) {
   let trackIndex = trackTileToIndex(trackTileCol, trackTileRow)
   return (trackGrid[trackIndex] === 1)
 }
 
 
-// function bounceOffTrackAtPixelCoord(pixelX, pixelY) {
-//   let tileCol = Math.floor(pixelX / TRACK_W);
-//   let tileRow = Math.floor(pixelY / TRACK_H);
-//   //проверяем находится ли мяч в районе кирпичей
-//   if (tileCol < 0 || tileCol >= TRACK_COLS || tileRow < 0 || tileRow >= TRACK_ROWS) {
-//     return
-//   }
+function checkForTrackAtPixelCoord(pixelX, pixelY) {
+  let tileCol = Math.floor(pixelX / TRACK_W);
+  let tileRow = Math.floor(pixelY / TRACK_H);
+  //проверяем находится ли мяч в районе кирпичей
+  if (tileCol < 0 || tileCol >= TRACK_COLS || tileRow < 0 || tileRow >= TRACK_ROWS) {
+    return false
+  }
 
-//   //индекс ячейки в которую ударили
-//   let trackIndex = trackTileToIndex(tileCol, tileRow);
-//   if (trackGrid[trackIndex] === 1) {
-//     let prevCarX = carX - carSpeedX;
-//     let prevCarY = carY - carSpeedY;
-//     let prevTileCol = Math.floor(prevCarX / TRACK_W);
-//     let prevTileRow = Math.floor(prevCarY / TRACK_H);
-//     let bothTestsFailed = true;
-//     //если попадаем и меняется колонка то отскакивает горизонтально
-//     if (prevTileCol != tileCol) {
-//       let adjacentTrackIndex = trackTileToIndex(prevTileCol, tileRow);//свободна или нет ячейка в prevTileCol для теста 
-//       if (trackGrid[adjacentTrackIndex] !== 1) {
-//         carSpeedX *= -1;
-//         bothTestsFailed = false;
-//       }
-//     }
-//     //если попадаем и меняется ряд то отскакивает вертикально
-//     if (prevTileRow != tileRow) {
-//       let adjacentTrackIndex = trackTileToIndex(tileCol, prevTileRow);//свободна или нет ячейка в prevTileCol для теста 
-//       if (trackGrid[adjacentTrackIndex] !== 1) {
-//         carSpeedY *= -1;
-//         bothTestsFailed = false;
-//       }
-//     }
-
-//     //если попадаем наискосок в место где с двух сторон стоят клетки 
-//     if (bothTestsFailed) {
-//       carSpeedY *= -1;
-//       carSpeedX *= -1;
-//     }
-//     //удаляем кирпич
-//   }
-// }
+  //индекс ячейки в которую ударили
+  let trackIndex = trackTileToIndex(tileCol, tileRow);
+  return (trackGrid[trackIndex] === TRACK_ROAD)
+}
 
 function trackTileToIndex(tileCol, tileRow) {
   return (tileCol + TRACK_COLS * tileRow)
